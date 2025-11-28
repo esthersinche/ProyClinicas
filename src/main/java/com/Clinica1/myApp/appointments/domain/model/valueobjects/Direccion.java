80476fa16fc4658d9d124d259efb36de3cf16eb5
Laura <lydiaberry2003@gmail.com>
2025-11-28 13:39:44 -0500

    TODO: falta actualizar los tests e interface(en un rato)

--- src/main/java/com/Clinica1/myApp/appointments/domain/model/valueobjects/Direccion.java
+++ src/main/java/com/Clinica1/myApp/appointments/domain/model/valueobjects/Direccion.java
@@ -2,28 +2,28 @@ package com.Clinica1.myApp.appointments.domain.model.valueobjects;
 
 import java.util.Objects;
 
-public record Direccion(String avenida, String calle, String distrito, String pais) {
+public record Direccion(String avenida, String distrito, String departamento, String provincia) {
 
     public Direccion{
         if (avenida == null || avenida.trim().isEmpty()){
             throw new IllegalArgumentException("Avenida cannot be null or Empty");
         }
 
-        if (calle == null || calle.trim().isEmpty()){
-            throw new IllegalArgumentException("Calle cannot be null or Empty");
-        }
-
         if (distrito == null || distrito.trim().isEmpty()){
             throw new IllegalArgumentException("Distrito cannot be null or Empty");
         }
 
-        if (pais == null || pais.trim().isEmpty()){
-            throw new IllegalArgumentException("País cannot be null or Empty");
+        if (departamento == null || departamento.trim().isEmpty()){
+            throw new IllegalArgumentException("Departamento cannot be null or Empty");
+        }
+
+        if (provincia == null || provincia.trim().isEmpty()){
+            throw new IllegalArgumentException("Provincia cannot be null or Empty");
         }
     }
 
-    public static Direccion of(String avenida, String calle, String distrito, String pais){
-        return new Direccion(avenida, calle, distrito, pais);
+    public static Direccion of(String avenida, String distrito, String departamento, String provincia){
+        return new Direccion(avenida, distrito, departamento, provincia);
     }
 
     //record tiene equals y hashcode auto
