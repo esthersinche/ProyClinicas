ef97d716107ca0cf5b97f9b707f640592b67a927
Laura <lydiaberry2003@gmail.com>
2025-12-01 20:52:42 -0500

    empleado creado
    contra?

--- src/main/java/com/Clinica1/myApp/IAMusuario/infrastructure/persistence/jpa/repository/JPAEmpleadoRepository.java
+++ src/main/java/com/Clinica1/myApp/IAMusuario/infrastructure/persistence/jpa/repository/JPAEmpleadoRepository.java
@@ -1,4 +1,15 @@
 package com.Clinica1.myApp.IAMusuario.infrastructure.persistence.jpa.repository;
 
-public interface JPAEmpleadoRepository {
+import com.Clinica1.myApp.IAMusuario.domain.repository.EmpleadoRepository;
+import com.Clinica1.myApp.IAMusuario.infrastructure.persistence.jpa.entity.EmpleadoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JPAEmpleadoRepository extends JpaRepository<EmpleadoEntity, String> {
+
+    Optional<EmpleadoEntity> findbyEmail (String email_empent);
+    Optional<EmpleadoEntity> findbyNamecompleto (String nom_empent, String ape_empent);
+    List<EmpleadoEntity> findbyRol (String idrol_empent);
 }
